26ef074a52682eb39cae1e75ff5bfc0775e78136
WangZhen-Ddm <846958884@qq.com>
2020-08-25 14:21:24 +0800

    fix: swagger-ui host

--- src/main/java/com/hyapp/restful/demo/config/SwaggerConfig.java
+++ src/main/java/com/hyapp/restful/demo/config/SwaggerConfig.java
@@ -20,7 +20,7 @@ public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .host("http://localhost:8082")
+                .host("poct.zjubme307.cn")
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.hyapp.restful.demo.controller"))
